7c61a0be3e397f9b4ee9a5bce284b77303801ede
Hernán García <hergarcia.d@gmail.com>
2021-10-03 22:01:09 -0300

    Tests nuevos: - is_mine - get_all_the_mines - get_mines_around

--- test/test_board.py
+++ test/test_board.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
-
-from board import convert_board_to_2d, get_initial_parameters, generate_board_1d, create_board
-
+from random import randint
+from board import convert_board_to_2d, get_initial_parameters, generate_board_1d, create_board, is_mine, get_all_the_mines, get_mines_around
 
 class Test(TestCase):
     def test_get_initial_parameters(self):
@@ -74,5 +73,81 @@ class Test(TestCase):
             mines_count += row.count(True)
         self.assertEqual(mines, mines_count)
 
+    def test_is_mine(self):
+        # given
+        x = 1
+        y = 2
+        board_2d = [[True, False], [True, False], [False, True], [False, True]]
+
+        # when
+        res = is_mine(board_2d, x, y)
+
+        # verify
+        self.assertEqual(res, {'mina': True, 'lista': [{'x': 0, 'y': 0}, {'x': 0, 'y': 1},{'x': 1, 'y': 2}, {'x': 1, 'y': 3}], 'numero': 0})
+
+    def test_is_mine2(self):
+        # given
+        x = 2
+        y = 2
+        board_2d = [[True, False], [True, False], [False, True], [False, True]]
+
+        # when
+        res = is_mine(board_2d, x, y)
+
+        # verify
+        self.assertIsNone(res)
+
+    def test_get_all_the_mines(self):
+        # given
+        level = randint(1, 10)
+        (time, width, long, mines) = get_initial_parameters(level)
+        board_2d = [[True, False, False], [False, True, False], [False, True, True], [True, False, True], [False, False, False]]
+
+        # when
+        board = create_board(level)
+        res = get_all_the_mines(board_2d)
+        res2 = get_all_the_mines(board)
+
+        # verify
+        self.assertEqual(res, [{'x': 0, 'y': 0}, {'x': 1, 'y': 1},{'x': 1, 'y': 2}, {'x': 2, 'y': 2}, {'x': 0, 'y': 3}, {'x': 2, 'y': 3}])
+        self.assertEqual(len(res2), mines)
+
+    def test_get_all_the_mines_False(self):
+        # given
+        board_2d_False = [[False, False], [False, False], [False, False], [False, False], [False, False]]
+
+        # when
+        resF = get_all_the_mines(board_2d_False)
+
+        # verify
+        self.assertEqual(resF, [])
 
 
+    def test_get_mines_around(self):
+        # given
+        level = randint(1, 10)
+        x = 1
+        y = 0
+
+        # when
+        board_2d = [[True, False, False], [False, True, False], [False, True, True], [True, False, True], [False, False, False]]
+        res = get_mines_around(board_2d, x, y)
+
+        # verify
+        self.assertEqual(res, 2)
+
+
+    def test_get_mines_around2(self):
+        # given
+        level = randint(1, 10)
+        (time, width, long, mines) = get_initial_parameters(level)
+        x = randint(0, long)
+        y = randint(0, width)
+
+        # when
+        board = create_board(level)
+        res = get_mines_around(board, x, y)
+
+        # verify
+        self.assertIsNotNone(res)
+        self.assertIn(res, range(0, 8))
